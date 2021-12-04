bd6e39216917b66481d159d1125dc8cfa4f65c4c
PabloGod1326 <juanks339@gmail.com>
2021-12-04 13:05:11 -0500

    Ajustes en el form

--- src/Components/UI/Singin/Singin.jsx
+++ src/Components/UI/Singin/Singin.jsx
@@ -11,7 +11,7 @@ export const Singin = () => {
 
                     <div className="headBoard">
                         <h1>Create New Account</h1>
-                        <h5>Already Registered? <a href="#">Log In here.</a></h5>
+                        <h5>Already Registered? <a href="#">Login here.</a></h5>
                     </div>
 
                     <div className="divForm">
@@ -21,11 +21,17 @@ export const Singin = () => {
                             <b><p>NAME</p></b>
                             <input type="text" name="name" ></input>
 
+                            <b><p>SURNAME</p></b>
+                            <input type="text" name="surname" ></input>
+
                             <b><p>EMAIL</p></b>
                             <input type="text" name="email" ></input>
 
                             <b><p>PASSWORD</p></b>
                             <input type="password" name="password" ></input>
+
+                            <b><p>MOBILE NUMBER</p></b>
+                            <input type="text" name="mobileNumber" ></input>
 
                             <b><p>DATE OF BIRTH</p></b>
                             <input type="date" name="date" ></input><br/><br/>
