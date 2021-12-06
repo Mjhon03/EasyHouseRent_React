3a43bc40ca31b6ad7f664cd484a64586c090f0ec
Karen Agudelo <kdagudelo70@misena.edu.co>
2021-12-06 20:25:44 +0100

    Creo plantilla y primer componente de la vista

---
+++ src/Components/UI/Select/Select.jsx
@@ -0,0 +1,20 @@
+import './Select.css';
+export const Select=()=>{
+    <>
+    <header>
+        <div className="title">
+            <h2>Most Popular</h2>
+        </div>
+        <div>
+            <select>
+            <option value="value1">Apartment</option>
+            <option value="value1">Estate</option>
+            <option value="value1">House</option>
+            <option value="value1">Farm</option>
+            </select>
+            <a href="#">Profile</a>
+        </div>
+
+    </header>
+    </>
+}
